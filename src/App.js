e790884fc8f13795b23bc7536f044cf9e32a7703
Fabio Bier <fab.bier@gmail.com>
2017-08-22 16:26:51 +0200

    Created a really simple React component base for Cards.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import cardImage from './img/products/bio-broccoli.jpg';
+import Card from './components/card/card';
 import './styles/app.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="card">
-          <div className="card-content">
-              <div className="card-image">
-                  <img src={cardImage} alt="AH Biologische broccoli" />
-              </div>
-              <div className="card-title">AH Biologische broccoli</div>
-          </div>
-      </div>
+        <div>
+            <Card/>
+        </div>
     );
   }
 }
