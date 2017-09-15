ea99a6c52bc0a15058a5e451ea8fa769036df170
Auke ten Hoopen <auke.hoopen.ten@ahold.com>
2017-09-15 11:19:34 +0200

    Added and updated some unit test for the product controls container

--- src/__tests__/product.controls.test.js
+++ src/__tests__/product.controls.test.js
@@ -19,101 +19,173 @@ describe('', () => {
     //     });
     // });
     //
-    // describe('Saving the quantity on the server', () => {
-    //
-    //     describe('When the quantity is greater than the old quantity', () => {
-    //
-    //         it('should trigger an event', () => {
-    //             expect(true).toBe(false);
-    //         });
-    //     });
-    //
-    //     describe('When saving has succeeded', () => {
-    //         describe('when quantity is not zero', () => {
-    //
-    //             it('should trigger an event', () => {
-    //                 expect(true).toBe(false);
-    //             });
-    //         });
-    //
-    //         describe('when quantity is zero', () => {
-    //
-    //             it('should trigger an event', () => {
-    //                 expect(true).toBe(false);
-    //             });
-    //         });
-    //     });
-    //
-    //     describe('When saving has failed', () => {
-    //
-    //         it('should trigger an event', () => {
-    //             expect(true).toBe(false);
-    //         });
-    //     });
-    //
-    // });
-    //
-    // describe('Get quantity to increment', () => {
-    //     describe('when model has no add quantity', () => {
-    //         beforeAll(() => {
-    //             wrapper = renderControlsContainer();
-    //         });
-    //
-    //         it('should use the default quantity', () => {
-    //             clickPlusButton(wrapper);
-    //
-    //             expect(wrapper.state().quantity).toBe(1);
-    //         });
-    //     });
-    //
-    //     describe('When model has add quantity', () => {
-    //         beforeAll(() => {
-    //             wrapper = renderControlsContainer();
-    //         });
-    //
-    //         it('should use add quantity', () => {
-    //             wrapper.setState({incrementAmount: 10});
-    //             clickPlusButton(wrapper);
-    //
-    //             expect(wrapper.state().quantity).toBe(10);
-    //         });
-    //
-    //     });
-    // });
-    //
-    // describe('Decrement quantity', () => {
-    //
-    //     describe('when quantity is above zero', () => {
-    //         beforeAll(() => {
-    //             wrapper = renderControlsContainer();
-    //         });
-    //
-    //         it('should decrement quantity', () => {
-    //             wrapper.setState({quantity: 10});
-    //             clickMinButton(wrapper);
-    //
-    //             expect(wrapper.state().quantity).toBe(9);
-    //         });
-    //     });
-    //
-    //     describe('when quantity is zero', () => {
-    //         beforeAll(() => {
-    //             wrapper = renderControlsContainer();
-    //         });
-    //
-    //         it('should delete product from shopping list', () => {
-    //             wrapper.setState({quantity: 1});
-    //
-    //             const instance = wrapper.instance();
-    //             const spy = jest.spyOn(instance, 'deleteFromShoppingList').mockImplementation(() => true);
-    //
-    //             clickMinButton(wrapper);
-    //
-    //             expect(spy).toHaveBeenCalledTimes(1);
-    //         });
-    //     });
-    //
-    // });
+    describe('Saving the quantity on the server', () => {
+        describe('When the quantity is greater than the old quantity', () => {
+            it('should trigger an quantity increased event', () => {
+                const onChangeSpy = jest.fn();
+                const wrapper = mount(
+                    <ProductControlsContainer onStateChange={onChangeSpy}>
+                        {({getPlusProps}) => (
+                            <div>
+                                <PlusButton {...getPlusProps()} />
+                            </div>
+                        )}
+                    </ProductControlsContainer>,
+                );
+
+                const button = wrapper.find(testhookId('plus'));
+                button.simulate('click');
+
+                expect(onChangeSpy).toHaveBeenCalledTimes(1);
+                expect(onChangeSpy).toHaveBeenLastCalledWith(ProductControlsContainer.stateChangeEvents.QUANTITY_INCREASED, 1);
+            });
+        });
+
+        describe('When saving has succeeded', () => {
+            describe('when quantity is not zero', () => {
+                it('should trigger an event', () => {
+                    const onChangeSpy = jest.fn();
+                    const wrapper = mount(
+                        <ProductControlsContainer onStateChange={onChangeSpy} quantity={10}>
+                            {({getMinProps}) => (
+                                <div>
+                                    <MinButton {...getMinProps()} />
+                                </div>
+                            )}
+                        </ProductControlsContainer>,
+                    );
+
+                    wrapper.instance().saveQuantity = jest.fn();
+                    // console.log( wrapper.instance());
+                    wrapper.setState({ quantity: 0});
+
+                    const button = wrapper.find(testhookId('min'));
+                    button.simulate('click');
+
+                    expect(wrapper.instance().saveQuantity).toHaveBeenCalledTimes(1);
+                });
+            });
+
+            describe('when quantity is zero', () => {
+                it('should trigger an delete from shoppinglist event', () => {
+                    const onChangeSpy = jest.fn();
+                    const wrapper = mount(
+                        <ProductControlsContainer onStateChange={onChangeSpy} quantity={1}>
+                            {({getMinProps}) => (
+                                <div>
+                                    <MinButton {...getMinProps()} />
+                                </div>
+                            )}
+                        </ProductControlsContainer>,
+                    );
+
+                    const button = wrapper.find(testhookId('min'));
+                    button.simulate('click');
+
+                    expect(onChangeSpy).toHaveBeenCalledTimes(1);
+                    expect(onChangeSpy).toHaveBeenLastCalledWith(ProductControlsContainer.stateChangeEvents.DELETED_FROM_SHOPPINGLIST);
+
+                });
+            });
+        });
+
+        describe('When saving has failed', () => {
+            it('should trigger an event', () => {
+                const onChangeSpy = jest.fn();
+                const fetch = () => new Promise((resolve, reject) => {
+                    reject();
+                });
+                const wrapper = mount(
+                    <ProductControlsContainer fetch={fetch} onStateChange={onChangeSpy} quantity={10}>
+                        {({getMinProps}) => (
+                            <div>
+                                <MinButton {...getMinProps()} />
+                            </div>
+                        )}
+                    </ProductControlsContainer>,
+                );
+
+                wrapper.instance().saveQuantity = jest.fn();
+                const button = wrapper.find(testhookId('min'));
+                button.simulate('click');
+
+                expect(wrapper.instance().saveQuantity).toHaveBeenCalledTimes(1);
+                expect(onChangeSpy).toHaveBeenLastCalledWith(ProductControlsContainer.stateChangeEvents.DELETED_FROM_SHOPPINGLIST);
+            });
+        });
+
+    });
+
+    describe('Get quantity to increment', () => {
+        describe('when model has no add quantity', () => {
+            it('should use the default quantity', () => {
+                const {Component, childSpy} = setup();
+                const wrapper = mount(<Component />);
+                const button = wrapper.find(testhookId('plus'));
+                button.simulate('click');
+
+                expect(childSpy).toHaveBeenLastCalledWith(
+                    expect.objectContaining({quantity: 1}),
+                );
+            });
+        });
+
+        describe('When model has add quantity', () => {
+            it('should use add quantity', () => {
+                const {Component, childSpy} = setup();
+                const wrapper = mount(<Component incrementAmount={10} />);
+                const button = wrapper.find(testhookId('plus'));
+
+                button.simulate('click');
+
+                expect(childSpy).toHaveBeenLastCalledWith(
+                    expect.objectContaining({quantity: 10}),
+                );
+            });
+        });
+    });
+
+    describe('Decrement quantity', () => {
+        var wrapper;
+
+        beforeAll(() => {
+
+        });
+
+        describe('when quantity is above zero', () => {
+            it('should decrement quantity', () => {
+                const { Component, childSpy} = setup();
+                const wrapper = mount(<Component quantity={10} />);
+                const button = wrapper.find(testhookId('min'));
+                button.simulate('click');
+
+                expect(childSpy).toHaveBeenLastCalledWith(
+                  expect.objectContaining({ quantity: 9})
+                );
+            });
+        });
+
+        describe('when quantity is zero', () => {
+            it('should delete product from shopping list', () => {
+                const wrapper = mount(
+                    <ProductControlsContainer quantity={1}>
+                        {({getMinProps}) => (
+                            <div>
+                                <MinButton {...getMinProps()} />
+                            </div>
+                        )}
+                    </ProductControlsContainer>,
+                );
+
+                const button = wrapper.find(testhookId('min'));
+                wrapper.instance().deleteFromShoppingList = jest.fn();
+                button.simulate('click');
+
+                expect(wrapper.instance().deleteFromShoppingList).toHaveBeenCalledTimes(1);
+            });
+        });
+
+    });
 
 });
 
@@ -133,39 +205,27 @@ describe('Decrement quantity', () => {
         });
 
 
-        describe('when quantity is zero', () => {
-            it('should delete product from shopping list', () => {
-                const {Component, childSpy} = setup();
-                const wrapper = mount(<Component quantity={10}/>);
-                console.log(wrapper);
-
-                wrapper.setState({isSaving: true});
-
-                const button = wrapper.find(testhookId('min'));
-                button.simulate('click');
-
-                expect(childSpy).toHaveBeenLastCalledWith(
-                    expect.objectContaining({quantity: 10}),
-                );
-
-                // const instance = wrapper.instance();
-                // const spy = jest.spyOn(instance, 'deleteFromShoppingList').mockImplementation(() => true);
-                //
-                //
-                // expect(spy).toHaveBeenCalledTimes(1);
-            });
-        });
-    });
-
-    it('clicking on the button opens and closes the menu', () => {
-        const {Component, childSpy} = setup();
-        const wrapper = mount(<Component />);
-        const button = wrapper.find(testhookId('plus'));
-        button.simulate('click');
-
-        expect(childSpy).toHaveBeenLastCalledWith(
-            expect.objectContaining({quantity: 1}),
-        );
+        // describe('when quantity is zero', () => {
+        //     it('should delete product from shopping list', () => {
+        //         const {Component, childSpy} = setup();
+        //         const wrapper = mount(<Component quantity={10}/>);
+        //
+        //         wrapper.setState({isSaving: true});
+        //
+        //         const button = wrapper.find(testhookId('min'));
+        //         button.simulate('click');
+        //
+        //         expect(childSpy).toHaveBeenLastCalledWith(
+        //             expect.objectContaining({quantity: 10}),
+        //         );
+        //
+        //         // const instance = wrapper.instance();
+        //         // const spy = jest.spyOn(instance, 'deleteFromShoppingList').mockImplementation(() => true);
+        //         //
+        //         //
+        //         // expect(spy).toHaveBeenCalledTimes(1);
+        //     });
+        // });
     });
 
     it('getPlusProps returns all given props', () => {
